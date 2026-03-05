bebd165178dade9a08b0d8d2ee1abae7190d1d1b
Chris Ware <c.p.ware@hotmail.com>
2026-03-04 17:02:04 -0700

    ui updates

--- src/app/components/HomePage/Solution/Solution.tsx
+++ src/app/components/HomePage/Solution/Solution.tsx
@@ -19,8 +19,7 @@ export default function Solution() {
               <div className={styles.topLeft}>
                 <SectionIntro text='Solution' />
                 <h2 className={styles.heading}>
-                  We Build You the Platform, You Run
-                  Your Business.
+                  We Build You the Platform, You Run Your Business.
                 </h2>
               </div>
               <div className={styles.topRight}>
@@ -44,38 +43,45 @@ export default function Solution() {
               </div>
             </div>
             <div className={styles.bottom}>
-              {SolutionData.map((x) => (
-                <div className={styles.card} key={x.id}>
-                  <div className={styles.cardLeft}>
-                    <div className={styles.cardLeftTop}>
-                      <span className={styles.oneWordDesc}>
-                        0{x.id} {x.oneWordDesc}
-                      </span>
+              <div className={styles.mapDataContainer}>
+                {SolutionData.map((x) => (
+                  <div className={styles.card} key={x.id}>
+                    <div className={styles.cardLeft}>
+                      <div className={styles.cardLeftTop}>
+                        <span className={styles.oneWordDesc}>
+                          0{x.id} {x.oneWordDesc}
+                        </span>
                         <h3 className={styles.cardHeading}>{x.title}</h3>
+                      </div>
+                      <div className={styles.cardLeftBottom}>
+                        <ul className={styles.bulletList}>
+                          {x.bullets.map((bullet, index) => (
+                            <li
+                              key={index}
+                              className={`${styles.bulletTitle} h3`}
+                            >
+                              {bullet}
+                            </li>
+                          ))}
+                        </ul>
+                      </div>
                     </div>
-                    <div className={styles.cardLeftBottom}>
-                      <ul className={styles.bulletList}>
-                        {x.bullets.map((bullet, index) => (
-                          <li key={index} className={`${styles.bulletTitle} h3`}>{bullet}</li>
-                        ))}
-                      </ul>
+                    <div className={styles.cardRight}>
+                      <div className={styles.imgContainer}>
+                        <Image
+                          src={x.src}
+                          alt={x.title}
+                          title={x.title}
+                          fill
+                          className={styles.img}
+                        />
+                        <div className={styles.imgOverlay} />
+                        <p className={styles.imgDescription}>{x.description}</p>
+                      </div>
                     </div>
                   </div>
-                  <div className={styles.cardRight}>
-                    <div className={styles.imgContainer}>
-                      <Image
-                        src={x.src}
-                        alt={x.title}
-                        title={x.title}
-                        fill
-                        className={styles.img}
-                      />
-                      <div className={styles.imgOverlay} />
-                      <p className={styles.imgDescription}>{x.description}</p>
-                    </div>
-                  </div>
-                </div>
-              ))}
+                ))}
+              </div>
             </div>
           </div>
         </div>
