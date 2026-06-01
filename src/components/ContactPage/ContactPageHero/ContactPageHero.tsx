17ae35cfae60d97c44de76f4f7d06c3782e23943
Chris Ware <c.p.ware@hotmail.com>
2026-05-31 19:58:08 -0700

    contact ui updates

--- src/components/ContactPage/ContactPageHero/ContactPageHero.tsx
+++ src/components/ContactPage/ContactPageHero/ContactPageHero.tsx
@@ -24,40 +24,38 @@ export default function ContactPageHero() {
                   <span className={styles.accent}>a conversation</span>
                 </h1>
               </div>
-              <div className={styles.b2}>
-                <div className={styles.b2Left}>
-                  <h3 className={styles.subHeadinigh}>General Questions</h3>
-                  <p className={styles.copy}>
-                    If you’re exploring Strativ or need clarity about how it
-                    works, feel free to reach out.
-                    <span className={styles.copyAccent}>
-                      We’re here to help you understand whether Strativ is the
-                      right fit for your team.
-                    </span>
-                  </p>
-                  <div>
-                    <p className={styles.emailLinkHeading}>Email</p>
-                    <a className={styles.emailLink}>
-                      hello@fontsandfooters.com
-                    </a>
-                  </div>
-                  <div>
-                    <p className={styles.emailLinkHeading}>Office Hours</p>
-                    <a className={styles.emailLink}>
-                      Monday - Friday
-                      <br />
-                      9:00 AM - 6:00 PM (CET)
-                    </a>
-                  </div>
-                  <div>
-                    <p className={styles.emailLinkHeading}>Socials</p>
-                    <a className={styles.emailLink}>Socials here</a>
-                  </div>
-                </div>
-                <div className={styles.b2Right}></div>
-              </div>
             </div>
           </div>
+        </div>
+        <div className={styles.b2}>
+          <div className={styles.b2Left}>
+            <h3 className={styles.subHeadinigh}>General Questions</h3>
+            <p className={styles.copy}>
+              If you’re exploring Strativ or need clarity about how it works,
+              feel free to reach out.{" "}
+              <span className={styles.copyAccent}>
+                We’re here to help you understand whether Strativ is the right
+                fit for your team.
+              </span>
+            </p>
+            <div>
+              <p className={styles.emailLinkHeading}>Email</p>
+              <a className={styles.emailLink}>hello@fontsandfooters.com</a>
+            </div>
+            <div>
+              <p className={styles.emailLinkHeading}>Office Hours</p>
+              <a className={styles.emailLink}>
+                Monday - Friday
+                <br />
+                9:00 AM - 6:00 PM (CET)
+              </a>
+            </div>
+            <div>
+              <p className={styles.emailLinkHeading}>Socials</p>
+              <a className={styles.emailLink}>Socials here</a>
+            </div>
+          </div>
+          <div className={styles.b2Right}></div>
         </div>
       </LayoutWrapper>
     </section>
